05866ef7eba5982b8c65c159c8e30520cbcbc89a
nmaeda38 <naoya_maeda38@yahoo.co.jp>
2025-07-28 14:17:05 +0900

    change aaa.m as script file.

--- aaa.m
+++ aaa.m
@@ -1,16 +1,4 @@
-function [outputArg1,outputArg2] = aaa(inputArg1,inputArg2)
-%AAA この関数の概要をここに記述
-%   詳細説明をここに記述
-arguments (Input)
-    inputArg1
-    inputArg2
-end
+disp('Run aaa')
 
-arguments (Output)
-    outputArg1
-    outputArg2
-end
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+ver -support
+pwd
