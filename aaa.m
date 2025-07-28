54299447f1e33c550c46051fb8139d9d308e63b1
nmaeda38 <naoya_maeda38@yahoo.co.jp>
2025-07-28 13:59:33 +0900

    create aaa.m initially. 7/28

---
+++ aaa.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = aaa(inputArg1,inputArg2)
+%AAA この関数の概要をここに記述
+%   詳細説明をここに記述
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
